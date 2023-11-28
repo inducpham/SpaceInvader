b5b286ae1b29ee87827dd4796d3824f677fc9441
Duc Pham <inducpham@gmail.coom>
2023-11-28 07:24:47 +0700

    Feat: add scene mainmenu for locale settings

---
+++ Assets/SpaceInvaderTest/Scripts/UI/UIMainmenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceInvaderTest
+{
+    public class UIMainmenu : MonoBehaviour
+    {
+        [SerializeField] private UnityEngine.UI.Button buttonPlay;
+        [SerializeField] private UnityEngine.UI.Button buttonLocale;
+
+        [Header("Group Locale")]
+        [SerializeField] private GameObject groupLocale;
+        [SerializeField] private UnityEngine.UI.Button buttonLocaleEnglish;
+        [SerializeField] private UnityEngine.UI.Button buttonLocaleVietnamese;
+        [SerializeField] private UnityEngine.UI.Button buttonLocaleBack;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            buttonPlay.onClick.AddListener(PlayGame);
+            buttonLocale.onClick.AddListener(ShowGroupLocale);
+
+            buttonLocaleBack.onClick.AddListener(HideGroupLocale);
+            buttonLocaleEnglish.onClick.AddListener(() => ChangeLocale("en"));
+            buttonLocaleVietnamese.onClick.AddListener(() => ChangeLocale("vi"));
+        }
+
+        void ShowGroupLocale()
+        {
+            groupLocale.SetActive(true);
+        }
+
+        void HideGroupLocale()
+        {
+            groupLocale.SetActive(false);
+        }
+
+        void PlayGame()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Gameplay");
+        }
+
+        void ChangeLocale(string locale)
+        {
+        }
+
+    }
+}
